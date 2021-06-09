619d6714ed86ba9988c70c0813565f2653a0be4c
bbrad3 <68521389+bbrad3@users.noreply.github.com>
2021-06-09 12:46:02 -0600

    Added dotenv and axios dependencies, created .env file

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,6 +1,14 @@
+import axios from 'axios'
+import { useEffect } from 'react'
+
 import { Box, Heading, Text } from '@chakra-ui/react'
 
 function Products() {
+    // filter options (items/page)
+    // fetch limit items
+
+    useEffect()
+
     return (
         <Box>
             <Heading fontSize='3xl'>Products Page</Heading>
