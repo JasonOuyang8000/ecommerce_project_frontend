1a0effaf5bb21eff60cef09eaff0d97805752262
Jason Ouyang <jasonouyang8000@gmail.com>
2021-07-06 23:10:44 -0700

    cart page

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,12 +1,39 @@
 import { Container } from "@chakra-ui/react";
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 
 
 const CartPage = () => {
-    const [cartItems, setCartItems] = useState(JSON.parse(localStorage.getItem('cartItems')));
+    const [cartItems, setCartItems] = useState([]);
 
-    
+    const getCartItems = async() => {
+        try {
+            const items = JSON.parse(localStorage.getItem('cartItems'));
+
+          
+
+            const response = await axios.get(`${process.env.REACT_APP_BACKEND}/cart/items`, {
+                params: {
+                   items
+                }
+            });
+
+
+            console.log(response);
+            
+
+        }
+        catch(error) {
+            console.log(error);
+        }
+    }
+
+
+    useEffect(() => {
+        getCartItems();
+    }, [])
+
 
 
     return (
